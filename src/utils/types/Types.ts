b426285e4a1a9ff6199294ddf91b55ba57e85b1e
walrus811 <walrusray811@gmail.com>
2022-01-02 02:51:57 +0900

    except 주석 추가

--- src/utils/types/Types.ts
+++ src/utils/types/Types.ts
@@ -10,5 +10,5 @@ export interface QueryField
   lastId?: string;
   sortBy?: string;
   ascend?: -1 | 1;
-  except?: string; // "name" or "name"|"startDate"|"something"
+  except?: string; // "name" or "name|startDate|something"
 }
